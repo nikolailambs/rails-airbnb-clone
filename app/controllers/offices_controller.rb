147fbba64d5326505f86e0f260521136b55780ad
Christophe Vercarre <christophe.vercarre@gmail.com>
2017-11-21 16:47:16 +0100

    Add map layout to index

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -3,6 +3,13 @@ class OfficesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @offices = Office.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@offices) do |office, marker|
+      marker.lat office.latitude
+      marker.lng office.longitude
+      # marker.infowindow render_to_string(partial: "/offices/map_box", locals: { office: flat })
+    end
   end
 
   def show
