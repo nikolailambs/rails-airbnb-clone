9a934a16cc78e76adb497a2699a7cdabfc7c78cd
nikolai <nikolai.lambsdorff@gmx.de>
2017-11-22 14:54:07 +0100

    index and filters are nearly done, also search algorithm

--- app/assets/javascripts/init_attachinary.js
+++ app/assets/javascripts/init_attachinary.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
   $('.attachinary-input').attachinary();
 });
-
-
-$(function() {
-  $('.card-link').hover(function() {
-    $('.card-description').css('color', '#AA0003');
-  }, function() {
-    // on mouseout, reset the background colour
-    $('.card-description').css('color', '');
-  });
-});
