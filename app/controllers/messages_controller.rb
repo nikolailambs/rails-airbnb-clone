2c03b99ffdea9e4ceafb43a5bd3875c566cf07e2
Alex Paul <alex@roadsign.fr>
2017-11-22 14:01:01 +0100

    meassagecreatuionandread

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,18 @@ class MessagesController < ApplicationController
     @messages = Messages.all
   end
 
+  def show
+    @messages = Message.where(user_id: current_user.id).order(created_at: :desc)
+
+    if params[:office_id]
+      @messages = @messages.where(office_id: params[:office_id])
+      render 'index'
+    end
+  end
+
   def create
+    @messages = Message.new
   end
 end
+
+
